cf5ab22d12e83ece447312ca5be9ce5158e28936
moe1129peterson <71342567+moe1129peterson@users.noreply.github.com>
2021-10-24 23:53:20 -0600

    Update todo.js

--- routes/todo.js
+++ routes/todo.js
@@ -1,80 +1,48 @@
-const express = require('express'); 
-const router = express.Router();
-const uuid = require('uuid');
-const todos = require('../todo.json')
+const todolist = document.querySelector('.todolist'); 
+const addTaskButton = document.getElementById('addTaskButton');
+const newTaskName = document.getElementById('newTaskName');
+let output = '';
 
-//get all todos including categories
-router.get ('/', (req, res) => {
-    res.send(todos); 
-}); 
+const renderPosts = (posts) =>{
+    posts.forEach(post =>{
+        output += `
+            <div class= "card mt-4 col-md-6 bg-light">
+                <div class="card-body">
+                    <h5 class="card-title">${post.taskName}</h5>
+                    <p class="card-text">Category would be here. </p>
+                    <a href="#" class="card-link">Edit</a>
+                    <a href="#" class="card-link">Delete</a>
+                </div>              
+            </div>
+            `;
+    });
 
-//creating unique id for todos
-router.post('/', (req, res) =>{
-    const todo = req.body;
-    todos.push ({ ...todo, id: uuid.v4()});
-    res.send (`User with the name ${todo.id}, ${todo.taskName}, ${todo.completed}, ${todo.category} added to the todo list`);
-});
+    todolist.innerHTML = output; 
+}
 
-//adding todos
-router.get ('/todo', (req, res) => {
-    const {id} = req.params;
-    const foundId = todos.find ((todo) => todo.id === id); 
-    res.send(foundId);
-});
+const url = 'http://localhost:5000/api/todos'; 
 
-//adding categories
-router.get ('/category', (req, res) => {
-    const {category} = req.params;
-    const foundCategory = todos.find ((todo) => todo.category === category); 
-    res.send(foundCategory);
-});
+//GET: Read posts 
+fetch(url)
+    .then(res => res.json())
+    .then(data => renderPosts (data))
 
-//deleting todo
-router.delete('/todo/:id', (req, res) => {
-    try {
-        const {id} = req.params
-        let index = todos.findIndex(todo => todo.id === parseInt(id));
-        if (index) {
-            todos.splice(index, 1)
-            res.send (`${id} Sucessfully deleted from the database.`);
-        }else{
-            res.send(`Todo with id ${id} does not exist`)
-        }
-    }catch(error){
-        res.status(500).send(error.message)
-    }
+//POST: Create posts 
+addTaskButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    fetch (url, {
+        method: 'POST', 
+        headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+        }, 
+        body: JSON.stringify({
+            taskName: newTaskName.value,
+        })
+    })
+        .then(res => res.json())
+        .then(data => {
+            const dataArr =[];
+            dataArr.push(data); 
+            renderPosts(dataArr);
+        })
 })
-
-//deleteing categories 
-router.delete('/category/:id', (req, res) => {
-    try {
-        const {id} = req.params
-        let index = todos.findIndex(todo => todo.id === parseInt(id));
-        if (index) {
-            todos.splice(index, 1)
-            res.send (`${id} Sucessfully deleted from the database.`);
-        }else{
-            res.send(`Todo with id ${id} does not exist`)
-        }
-    }catch(error){
-        res.status(500).send(error.message)
-    }
-})
-
-router.patch('/:id', (req, res) => {
-    const {id} = req.params;
-    const {taskName, category} = req.body; 
-    const todo =todos.find((todo) => todos.id ===id);
-
-    if(taskName){
-        todos.taskName = taskName;
-    }
-
-    if(category){
-        todos.category = category; 
-    }
-
-    res.send('Todo with the id ${id} has been updated.')
-
-})
-module.exports = router; 
